55e479f987f90099877bca040029ebce50a89c22
tibebesaas <louabi@hotmail.com>
2024-06-05 08:01:16 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -48,22 +48,6 @@ function watchFiles() {
   gulp.watch("./**/*.html", browserSyncReload);
 }
 
-function saveToFirebase(email) {
-    var emailObject = {
-        email: email
-    };
-
-    firebase.database().ref('users').push().set(emailObject)
-        .then(function(snapshot) {
-            success(); // some success method
-        }, function(error) {
-            console.log('error' + error);
-            error(); // some error method
-        });
-}
-
-saveToFirebase(email);
-
 // Define complex tasks
 const vendor = gulp.series(clean, modules);
 const build = gulp.series(vendor);
